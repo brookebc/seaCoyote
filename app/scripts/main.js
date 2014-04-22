86828d992316cbd645873e13d0d32f1d688f943c
brookebc <brooke.casey@gmail.com>
2014-04-21 22:28:52 -0400

    shopping cart started with buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,43 @@
-console.log('\'Allo \'Allo!');
+// product constructor
+
+// john- please get all the products to show up on the page 
+//with a picture, then the product properties listed below,
+// then a button in that box thhat they will click to add the product to the  "shopping cart" function that I will create.
+
+// customer or buyer constructor
+
+// customer review constructor
+
+// customer review array
+
+function yourtotal = function(data){
+	var markup = "";
+	var i;
+	this.data= data;
+
+	var totaled = $(this).val()):
+
+
+	for (i=0; i < data.length; i++) {
+  	console.log("looping")
+    markup += "<li>" + data[i].pic +  "</li>";
+  }
+  return markup;
+}
+
+var newCarPicMarkup = carpic(carsArray)
+$(".carchoices").append(newCarPicMarkup);
+
+}
+
+ $(".calcutlate-total").click(multiplyTheFields();
+ 	
+$(".buyit")click(function(){
+	event.preventDefault;
+
+	var trip = Number($(this).val());
+	console.log(trip);
+	$(".destinationselected").append("<span>" + destinationArray[trip].minutes + "</span>");
+}
+
+
