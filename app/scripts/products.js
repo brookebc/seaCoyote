c1787afba4112ef3acc77ec2e87c6643f34b491d
popeonarope <johnvepope@gmail.com>
2014-04-21 22:34:42 -0400

    styled buttons, edited buttons to make them more accessible to cart constructor

--- app/scripts/products.js
+++ app/scripts/products.js
@@ -7,10 +7,10 @@ function Product(pic, name, color, cost){
 	this.cost = cost;
 }
 
-var puddleJumper = new Product("<img src='../img/kayak0.png'>", "Puddle-Jumper", "Red", "$600");
-var riverMonster = new Product("<img src='../img/kayak1.png'>", "River Monster", "Yellow", "$400");
-var greenGoblin = new Product("<img src='../img/kayak2.png'>", "Green Goblin", "Green", "$700");
-var spiderMan = new Product("<img src='../img/kayak3.png'>", "Spider-Man", "Red and White", "$900");
+var puddleJumper = new Product("<img src='../img/kayak0.png'>", "Puddle-Jumper", "Red", 600);
+var riverMonster = new Product("<img src='../img/kayak1.png'>", "River Monster", "Yellow", 400);
+var greenGoblin = new Product("<img src='../img/kayak2.png'>", "Green Goblin", "Green", 700);
+var spiderMan = new Product("<img src='../img/kayak3.png'>", "Spider-Man", "Red and White", 900);
 
 var productArray = [puddleJumper, riverMonster, greenGoblin, spiderMan];
 
@@ -21,13 +21,15 @@ Page.prototype.productTemplate = function(data){
 		markup += "<div class = 'productBox'>" + 
 		data[i].pic + "<ul><li><h4>" + 
 		data[i].name + "</h4></li><li>Color: " + 
-		data[i].color + "</li><li>Price: " + 
+		data[i].color + "</li><li>Price: $" + 
 		data[i].cost + "</li><li><button class = 'btn-lg' id = 'buyNow'>Buy it now!</button></li></ul></div>"
 		
 		console.log(i);
 	}
 	return markup;
 };
+
+
 
 Page.prototype.sendToDom = function($el, data){
 	$el.append(this.productTemplate(data));
