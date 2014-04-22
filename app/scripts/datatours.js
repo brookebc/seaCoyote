95eb5bbf658be8b93bb125e60679da8f5c3bb5e8
brookebc <brooke.casey@gmail.com>
2014-04-22 19:23:12 -0400

    oh yeah the underscore is working

--- app/scripts/datatours.js
+++ app/scripts/datatours.js
@@ -1,73 +1,33 @@
-<h2>SC Canoeing, Kayaking, and Paddling</h2>
+var sctours = [
+{
+	"citytour": {
+	"city": "Beaufort",
+	"name": "Beaufort Kayak Tours",
+	"link": "http://www.beaufortkayaktours.com/",
+		}
+	},
+{ 
+	"citytour": {
+	"city": "Charleston",
+ 	"name": "Coastal Expeditions",
+	"link": "http://www.coastalexpeditions.com",  
+		}
+	},
 
-<!--formb-->
-<script src="/js/jquery-1.2.3.pack.js"></script>
-<script src="/js/linkform.js"></script>
-<div id="contact_form" align=left>
-<form name="contact" method="post" action="">
-<fieldset>
-<label for="name" id="name_label"><img class=plusgreen src="/i/plus-new.gif"></label>
-<input type="text" name="name" id="name" size="30" class="text-input" onfocus="if(this.value=='Add a website (URL) here'){this.value='';}this.style.color='';" onblur="if (this.value==''){this.style.color='';this.value='Submit a website (URL) here';}" value="Add a website (URL) here" size="30">
-<input type="submit" name="submit" class="button" id="submit_btn" value="ADD" />
-</fieldset>
-</form>
-</div><!--forme-->
+{ 
+	"citytour": {
+	"city": "Columbia",
+	"name": "Adventure Carolina",
+	"link": "http://www.adventurecarolina.com/",
+	}
+},
 
-<h3>ACE Basin Kayak, Canoe</h3>
-<ul>
-<li><a href="ace-basin-canoe-kayak-nature-tours.html">Kayaking in the ACE Basin</a> - Ashepoo, Combahee, Edisto Rivers
-</ul>
+{ 
+	"citytour": {
+	"city": "Florence Kayak",
+	"name": "RiverRats Canoe Rentals",
+	"link": "http://www.riverratscanoerentals.com/",  
+		}
+	}
+];
 
-<h3>Beaufort Kayak, Canoe</h3>
-<ul>
-<li><a href="http://www.beaufortkayaktours.com/">Beaufort Kayak Tours</a>
-</ul>
-
-<h3>Charleston Kayak, Canoe</h3>
-<ul>
-<li><a href="http://www.coastalexpeditions.com">Coastal Expeditions</a> - Mount Pleasant
-<li><a href="http://lowcountrypaddlers.net/">LowCountry Paddlers</a> - Daniel Island
-<li><a href="http://www.kayakcharlestonsc.com/">Nature Adventures Outfitters</a> - Mount Pleasant
-</ul>
-
-<h3>Columbia Kayak, Canoe</h3>
-<ul>
-<li><a href="http://www.adventurecarolina.com/">Adventure Carolina</a> - Cayce
-<li><a href="http://www.riverrunner.us/">River Runner Outdoor Center</a>
-</ul>
-
-<h3>Florence Kayak, Canoe</h3>
-<ul>
-<li><a href="http://www.riverratscanoerentals.com/">RiverRat's Canoe Rentals</a> - Scranton
-</ul>
-
-<h3>Georgetown Kayak, Canoe</h3>
-<ul>
-<li><a href="http://www.blackriveroutdoors.com/">Black River Outdoors Center</a>
-</ul>
-
-<h3>Greenwood Kayak, Canoe</h3>
-<ul>
-<li><a href="http://www.calmwaterkayaktours.com/">Calm Water Kayak Tours</a>
-</ul>
-
-<h3>Hilton Head Kayak, Canoe</h3>
-<ul>
-<li><a href="hhicanoe.html">Kayaking in Hilton Head</a>
-</ul>
-
-<h3>Lake Jocassee Kayak, Canoe</h3>
-<ul>
-<li><a href="http://eclecticsun.com/">Eclectic Sun</a> - Salem
-</ul>
-
-<h3>Moncks Corner Kayak, Canoe</h3>
-<ul>
-<li><a href="http://www.berkeleyblueways.com/">Berkeley County's BlueWays</a> - 23 self-guided trips<br>
-<li><a href="http://www.bluewayadventures.com/">Blueway Adventures</a> - Bonneau Beach<br>
-</ul>
-
-<h3>Myrtle Beach Kayak, Canoe</h3>
-<ul>
-<li><a href="http://www.jandlkayaking.com/north-myrtle-beach-kayaking.html">J &amp; L Kayaking</a> - North Myrtle Beach<br>
-</ul>
