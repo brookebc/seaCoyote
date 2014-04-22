c0a806c27f7469ba9e3b7cab2b0f0a48fc719979
popeonarope <johnvepope@gmail.com>
2014-04-21 22:04:41 -0400

    added buy it now button with id buyNow

--- app/scripts/products.js
+++ app/scripts/products.js
@@ -10,14 +10,20 @@ function Product(pic, name, color, cost){
 var puddleJumper = new Product("<img src='../img/kayak0.png'>", "Puddle-Jumper", "Red", "$600");
 var riverMonster = new Product("<img src='../img/kayak1.png'>", "River Monster", "Yellow", "$400");
 var greenGoblin = new Product("<img src='../img/kayak2.png'>", "Green Goblin", "Green", "$700");
+var spiderMan = new Product("<img src='../img/kayak3.png'>", "Spider-Man", "Red and White", "$900");
 
-var productArray = [puddleJumper, riverMonster, greenGoblin];
+var productArray = [puddleJumper, riverMonster, greenGoblin, spiderMan];
 
 Page.prototype.productTemplate = function(data){
 	var i;
 	var markup = "";
 	for(i=0; i<data.length; i=i+1){
-		markup += "<div class = 'productBox'>" + data[i].pic + "<ul><li><h4>" + data[i].name + "</h4></li><li>" + data[i].color + "</li><li>Price: " + data[i].cost + "</li></ul></div>"
+		markup += "<div class = 'productBox'>" + 
+		data[i].pic + "<ul><li><h4>" + 
+		data[i].name + "</h4></li><li>Color: " + 
+		data[i].color + "</li><li>Price: " + 
+		data[i].cost + "</li><li><button class = 'btn-lg' id = 'buyNow'>Buy it now!</button></li></ul></div>"
+		
 		console.log(i);
 	}
 	return markup;
