f3cf7c4983cde79869107a17828e2f8679af4e95
andrew8888 <andrew888@gmail.com>
2014-04-24 09:04:27 -0400

    changed format to comments and built working comment section

--- app/scripts/reviews.js
+++ app/scripts/reviews.js
@@ -14,7 +14,7 @@ Section.prototype.generateReview = function(data){
 	this.data = data;
 
 	for(i=0; i < data.length; i++) {
-		markup += "<h3>" + data[i].name + "</h3><li>" + data[i].comment + "</li>";
+		markup += "<li>" + data[i].comment + "</li>";
 	}
 	console.log(markup);
 	return markup;
@@ -22,8 +22,8 @@ Section.prototype.generateReview = function(data){
 
 var reviews = new Section();
 
-var bob = new Review("Bob", "The staff is very knowledgable and friendly! I would definitely buy from them again!");
-var jim = new Review("Jim", "I bought the spiderman model and it is very fast but unstable! Watch out!");
+var bob = new Review("Bob", "The staff is very knowledgable and friendly! I would definitely buy from them again! - Bob");
+var jim = new Review("Jim", "I bought the spiderman model and it is very fast but unstable! Watch out! -Jim ");
 
 
 var reviewArray= [bob,jim];
@@ -34,27 +34,9 @@ $(".past-reviews").append(reviews.generateReview(reviewArray));
 
 
 
-// var comment = 
 
 
 
 
-
-
-// function Comment(){};
-
-// function CustReview = new Review(name, comment);
-
-
-/*just for functionality...----------------------
-      var value = $('#name').val();
-   $('ul').append('<li>' + value + '</li>'); });
-*/
-//html code for button
-/*
-   <input id='name' type='text'>
-   <button id='addOne'>
-      Add One
-   </button>
-   --------------------------------------------- */
+  
 
