696e36f03722fbcefcea5537f102737368c30fb3
brookebc <brooke.casey@gmail.com>
2014-04-29 09:59:47 -0400

    adding the testing pages to the team project

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,11 +3,29 @@
 (function () {
     'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+    describe('Constructors', function () {
+        describe('Product Constructor testing', function () {
+        	var testProduct = new Product("../img/kayak0.png", "slingshotter", "purple", 200 );
+            
+            it('testProduct should be an instance of Product', function () {
+            	expect(testProduct).to.be.an.instanceof(Product);
 
             });
+
+            it("instance properties should reflect what was passed in", function() {
+
+	        	expect(testProduct.pic).to.equal("../img/kayak0.png");
+				expect(testProduct.name).to.equal("slingshotter");
+				expect(testProduct.color).to.equal("purple");
+				expect(testProduct.cost).to.equal(200);
         });
+
+    })
+
+	   //could add another that says that if you create a new instance w/o properties that they are undefined     
+      
     });
+
+		// describe('Constructors', function () {
+		//         describe('maybe a bit more context here', function () {
 })();
